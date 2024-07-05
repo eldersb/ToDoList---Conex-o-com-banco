0baaf20c20072fef7a6c465a08f9cef2af0ee65e
eldersb <122701368+eldersb@users.noreply.github.com>
2024-07-05 11:10:10 -0300

    Finalizando lógica de buscar

--- paginas/buscar_tarefas.php
+++ paginas/buscar_tarefas.php
@@ -2,9 +2,15 @@
 
 include "./conexao.php";
 
-$result = $mysqli->query("SELECT tarefa FROM tarefas");
-
 $tarefaBuscada = $_POST['buscar'] ?? "";
+
+if(empty($tarefaBuscada)){
+    $result = $mysqli->query("SELECT * FROM tarefas");
+} else {
+    $tarefaBuscada = $mysqli->real_escape_string($tarefaBuscada);
+    $result = $mysqli->query("SELECT * FROM tarefas WHERE tarefa LIKE '%$tarefaBuscada%'");
+    
+}
 
 
 ?>
@@ -14,28 +20,39 @@ $tarefaBuscada = $_POST['buscar'] ?? "";
             <div class="row">
                 <div class="col-md-12">
                     <h2 class="text-primary mt-5">Buscar Tarefas</h2>
-                    <form class="mt-2 mb-5" action="">
+                    <form class="mt-2 mb-5" action="" method="POST">
                         <div class="mt-2 d-flex justify-content-center align-items-end">
                             <input type="text" class="form-control" id="buscar" name="buscar" placeholder="Digite a tarefa a ser buscada">
                             <input type="submit" class="btn btn-primary mt-2" value="Buscar tarefa">
                         </div>
                     </form>
 
+                    <table class="table mt-4 table-hover table-bordered">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tarefa</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Ação</th>
+                </tr>
+
                     <?php 
-                    
-                    while($row = $result->fetch_assoc()){
-                        $tarefas = $row;
-                        foreach($tarefas as $task){
-                           if(str_contains(strtolower($task), strtolower($tarefaBuscada))){
-                                // $result2 = $mysqli->query("SELECT * FROM tarefas WHERE tarefa = $task;");
-                            }
-                           }
-                          
+                     
+                     if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()){
+                            echo "<tr>";
+                            echo "<td> " . $row['id'] . "</td>";
+                            echo "<td> " . $row['tarefa'] . "</td>";
+                            echo "<td> " . $row['dataTarefa'] . "</td>";
+                            echo "<td><a onclick='confirmarExclusao('') href='#'>Excluir </a></td>";
+                            echo "<tr>";
+                        }
                     }
-                    
+
+                
                     
                     ?>
-
+</table>
 
         </div>
+</div>
 </div>
